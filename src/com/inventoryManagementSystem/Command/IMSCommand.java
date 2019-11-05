1a84a075721fc7b8090834eeb577ee696dd39c03
Mark Lai <marklai1998@gmail.com>
2019-11-05 22:13:02 +0800

    chore: added comments

--- src/com/inventoryManagementSystem/Command/IMSCommand.java
+++ src/com/inventoryManagementSystem/Command/IMSCommand.java
@@ -5,6 +5,7 @@ public interface IMSCommand {
 
     void execute();
 
+//    permission = -1 mean it's a system command, it wont show on the main menu but can manually call(eg. login)
     int getMinPermission();
 
     String getDescription();
